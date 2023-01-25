04b3c75013fe45b6a7c943cb91c1ba1a5e7b00d2
Namthi Nguyen <nnguye29@students,towson.edu>
2023-01-25 04:07:16 -0500

    modify the code look and add more formating to the calculations

--- tipcaluclator.py
+++ tipcaluclator.py
@@ -8,25 +8,28 @@ description: Designing a tip calculator with more modification, such as printing
 print("**** Welcome to Nam's tip Calculator ****")
 
 # Ask user for their total bill and allow floating number and store it into a variable call customerbill
-customer_bill = float(input("What is the total bill? "))
+customer_bill = float(input("- What is the total bill? "))
 
 #need to store how many people to divide the bill by
-amt_of_people = int(input("how many people are willing to pay? "))
+amt_of_people = int(input("- how many people are willing to pay? "))
 
 # storing desire tip amount 
-desire_tip = float(input("What percentage tip would you like to give? 10, 12 or 18? "))
+desire_tip = float(input("- What percentage tip would you like to give? 10, 12 or 18? "))
 
 #Calculations for getting total bill before tax per person
 cost_per_person = (customer_bill) / (amt_of_people) 
+cost = "{:.2f}".format(cost_per_person)
 
 #Calculations for getting total bill with tac
-tip_CALC = (round(cost_per_person * desire_tip /100, 2 ))
-cost_per_person = "{:.2f}".format(tip_CALC)
+tip_per_person = (round(cost_per_person * desire_tip /100, 2 ))
+each_tip = "{:.2f}".format(tip_per_person)
 total_tip = (round(customer_bill * desire_tip /100, 2 ))
+combine_tip = "{:.2f}".format(total_tip)
 
 #Calculations for getting total bill per person to pay
 total_bill_per_person = (tip_per_person + cost_per_person)
+each_person_bill = "{:.2f}".format(total_bill_per_person)
 
 #Display the final message 
-print(f"*** Here is the breakdown of the bill *** \n\n * The cost per person = ${cost_per_person }, \n * Total tip per person = ${tip_per_person}, \n * Total bill per person is ${total_bill_per_person}, \n * You will have to pay ${total_tip} in tips")
-print("\n**** Thank you for using the calculator!! ****")
+print(f"*** Here is the breakdown of the bill *** \n\n * The total per person before tip = ${cost}, \n * You will have to pay ${combine_tip} in tips, \n * Total tip per person = ${each_tip}, \n * Total bill per person is ${each_person_bill}")
+print("\n*** Thank you for using the calculator!! ***")
